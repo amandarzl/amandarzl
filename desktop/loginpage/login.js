b51aacddca1cd427c799e5411f3838544c4c9a00
Amanda Ruazol <amandaruazol@gmail.com>
2026-07-22 17:05:03 +0800

    Cleaned up conflict markers

--- desktop/loginpage/login.js
+++ desktop/loginpage/login.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const loginButton = document.getElementById("loginButton");
 
 loginButton.addEventListener("click", () => {
-    window.location.href = "../desktop.html"
-=======
-const loginButton = document.getElementById("loginButton");
-
-loginButton.addEventListener("click", () => {
-    window.location.href = "../desktop.html"
->>>>>>> 85ffab3bead5f050ecdac1ff3af8a77c64971bf2
+  window.location.href = "../desktop.html";
 });
